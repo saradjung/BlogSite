9c2e4ba2d05315d6fd1160ded2017d88a51fdbb0
Rabin Shrestha <121755408+itsrawbin@users.noreply.github.com>
2025-02-21 19:31:45 +0545

    Single Blog Post Display Feature Added

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -7,3 +7,7 @@ from .models import Post
 def home(request):
     posts=Post.objects.all()
     return render(request,'home.html',{'posts':posts})
+
+def getBlog(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'singleblog.html',{'post':post})
