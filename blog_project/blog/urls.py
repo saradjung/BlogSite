d03a0918d093f2ebc8fd49719d1cc74d3c399936
Rabin Shrestha <121755408+itsrawbin@users.noreply.github.com>
2025-02-22 17:29:46 +0545

    About Page Added & Blog Post Feature From UI Introduced!

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home' ),
     path('blog/<int:post_id>', views.getBlog, name='blog'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('postblog/', views.postBlog, name='postblog'),
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
 
@@ -11,3 +11,19 @@ def home(request):
 def getBlog(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, 'singleblog.html',{'post':post})
+
+def postBlog(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        author = request.POST['author']
+        if title and content and author:
+            Post.objects.create(title=title, content=content, author=author)
+            return redirect('home')
+    return render(request, 'postblog.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
