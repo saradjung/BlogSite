82f03c456e7f0f43c69057840995e50ddaf31c55
Rabin Shrestha <121755408+itsrawbin@users.noreply.github.com>
2025-02-28 13:08:19 +0545

    Added Line Break on Blog Content

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -31,6 +31,9 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.user} commented on {self.post}"
+
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')  # Fix related_name
     user = models.ForeignKey(User, on_delete=models.CASCADE)
