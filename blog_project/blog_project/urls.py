2581e9ab274bab5720f6bf77142638a19095ada7
Sarad <thapasarad87@gmail.com>
2025-03-01 21:41:00 +0545

    used railway_volume_path for image handling v

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -22,4 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # For production, we still need to serve media files
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
