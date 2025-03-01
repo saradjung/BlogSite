f5ec5fdb4cbd8eb1bfb7d7ad8d7e813a1fc51e5c
Sarad <thapasarad87@gmail.com>
2025-03-01 21:09:32 +0545

    used railway_volume_path for image handling

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -18,6 +18,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+RAILWAY_VOLUME_PATH = os.getenv("RAILWAY_VOLUME_MOUNT_PATH", "/app/media")  # Default to /app/media if not set
+
 CSRF_TRUSTED_ORIGINS= ["https://blogsite-production-82f1.up.railway.app"]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -139,5 +141,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(RAILWAY_VOLUME_PATH, "media")
