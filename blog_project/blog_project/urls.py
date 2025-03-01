6bd3763b317c0c2c6b0df9ca1709279870222250
Sarad <thapasarad87@gmail.com>
2025-03-01 23:27:07 +0545

    modified urls and psskey for SU

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 from django.urls import re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminPanel/', admin.site.urls),
     path('', include('blog.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ] 
